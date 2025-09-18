9e3d3e26d5adea2dcd9791f3b2a11e0c63153e97
Jonathan Alexandro Pulido Estrada <jonathan712alex@outlook.com>
2025-09-18 10:18:32 -0600

    Implementando mockito en CRUD

---
+++ semana2/clientes-crud/clientes-crud/src/main/java/com/banco/clientes/ClientesCrudApplication.java
@@ -0,0 +1,13 @@
+package com.banco.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesCrudApplication.class, args);
+	}
+
+}
