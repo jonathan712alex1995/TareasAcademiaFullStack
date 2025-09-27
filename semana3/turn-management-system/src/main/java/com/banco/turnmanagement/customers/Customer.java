69efeb048f5461c33eda13c0e816ae1e6c4c6da0
Jonathan Alexandro Pulido Estrada <jonathan712alex@outlook.com>
2025-09-27 17:25:23 -0600

    eventos terminados

--- semana3/turn-management-system/src/main/java/com/banco/turnmanagement/customers/Customer.java
+++ semana3/turn-management-system/src/main/java/com/banco/turnmanagement/customers/Customer.java
@@ -1,5 +1,24 @@
 package com.banco.turnmanagement.customers;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable=false, unique=true)
+	private String document;
+	
+	@Column(nullable=false)
+	private String fullname;
 }
