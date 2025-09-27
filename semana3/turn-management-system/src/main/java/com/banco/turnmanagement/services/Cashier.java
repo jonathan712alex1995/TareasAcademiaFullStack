966d940bb12132792e8c569563b2f72fbd7b31c7
Jonathan Alexandro Pulido Estrada <jonathan712alex@outlook.com>
2025-09-27 17:22:01 -0600

    creados customers, events, services y turns

--- semana3/turn-management-system/src/main/java/com/banco/turnmanagement/services/Cashier.java
+++ semana3/turn-management-system/src/main/java/com/banco/turnmanagement/services/Cashier.java
@@ -1,5 +1,35 @@
 package com.banco.turnmanagement.services;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cashiers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cashier {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable=false)
+	@Enumerated(EnumType.STRING)
+	private ServiceType serviceType;
+	
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private CashierStatus cashierStatus;
+	
+	public enum ServiceType{
+		CAJA, EJECUTIVO
+	}
+	
+	public enum CashierStatus{
+		DISPONIBLE, OCUPADO, DESCANSO
+	}
 }
--- semana3/turn-management-system/src/main/java/com/banco/turnmanagement/turns/Turn.java
+++ semana3/turn-management-system/src/main/java/com/banco/turnmanagement/turns/Turn.java
@@ -1,5 +1,47 @@
 package com.banco.turnmanagement.turns;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "turns")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Turn {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private Long customerId;
+	
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private ServiceType serviceType;
+	
+	@Column(nullable = false , unique=true)
+	private String turnNumber;
+	
+	@Column(nullable=false)
+	@Enumerated(EnumType.STRING)
+	private TurnStatus status;
+	
+	@Column(nullable = false)
+	private LocalDateTime createdAt;
+	
+	private LocalDateTime calledAt;
+	
+	public enum ServiceType{
+		CAJA, EJECUTIVO
+	}
+	
+	public enum TurnStatus{
+		ESPERA, LLAMADO, ATENDIENDO, COMPLETADO, CANCELADO
+	}
 }
